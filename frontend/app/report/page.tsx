6fd69050a97b60de03cb196bd8870ed96e94ab8b
Anirudh Singh Chouhan <140189477+AnirudhSingh07@users.noreply.github.com>
2024-07-27 23:32:06 +0530

    reports

---
+++ frontend/app/report/page.tsx
@@ -0,0 +1,12 @@
+import Report  from '@/components/component/report'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Report/>
+    </div>
+  )
+}
+
+export default page
